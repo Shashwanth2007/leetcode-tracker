d5db20ba6c81c88744914bb54d394642c91882d5
Shashwanth2007 <shashwanthxox@gmail.com>
2026-08-31 09:24:58 +0530

    Create 34-FindFirstandLastPositionofElementinSortedArray

---
+++ 34-FindFirstandLastPositionofElementinSortedArray/version/java/34-FindFirstandLastPositionofElementinSortedArray_20260831_092456.java
@@ -0,0 +1,26 @@
+// Last updated: 8/31/2026, 9:24:56 AM
+1class Solution {
+2    public String countAndSay(int n) {
+3        String s = "1";
+4
+5        for (int k = 1; k < n; k++) {
+6            StringBuilder next = new StringBuilder();
+7
+8            for (int i = 0; i < s.length();) {
+9                int j = i;
+10
+11                while (j < s.length() && s.charAt(j) == s.charAt(i))
+12                    j++;
+13
+14                next.append(j - i);
+15                next.append(s.charAt(i));
+16
+17                i = j;
+18            }
+19
+20            s = next.toString();
+21        }
+22
+23        return s;
+24    }
+25}

a7f3e7eb9ba285bf404c63cd9e26df303a9c8cf0
Shashwanth2007 <shashwanthxox@gmail.com>
2026-08-31 09:55:38 +0530

    Create 58-LengthofLastWord

---
+++ 58-LengthofLastWord/version/java/58-LengthofLastWord_20260831_095536.java
@@ -0,0 +1,16 @@
+// Last updated: 8/31/2026, 9:55:36 AM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if (n <= 2) return n;
+4
+5        int a = 1, b = 2;
+6
+7        for (int i = 3; i <= n; i++) {
+8            int c = a + b;
+9            a = b;
+10            b = c;
+11        }
+12
+13        return b;
+14    }
+15}

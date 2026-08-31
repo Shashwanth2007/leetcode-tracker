a2e3c43506f02aae0aefc0042e0e79ecef695e2d
Shashwanth2007 <shashwanthxox@gmail.com>
2026-08-31 09:27:12 +0530

    Create 34-FindFirstandLastPositionofElementinSortedArray

---
+++ 34-FindFirstandLastPositionofElementinSortedArray/version/java/34-FindFirstandLastPositionofElementinSortedArray_20260831_092709.java
@@ -0,0 +1,24 @@
+// Last updated: 8/31/2026, 9:27:09 AM
+1class Solution {
+2    public int firstMissingPositive(int[] nums) {
+3        int n = nums.length;
+4
+5        for (int i = 0; i < n; i++) {
+6            while (nums[i] > 0 &&
+7                   nums[i] <= n &&
+8                   nums[nums[i] - 1] != nums[i]) {
+9
+10                int temp = nums[i];
+11                nums[i] = nums[temp - 1];
+12                nums[temp - 1] = temp;
+13            }
+14        }
+15
+16        for (int i = 0; i < n; i++) {
+17            if (nums[i] != i + 1)
+18                return i + 1;
+19        }
+20
+21        return n + 1;
+22    }
+23}
